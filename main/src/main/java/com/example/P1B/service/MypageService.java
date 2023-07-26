dff8da3afb3741b1f7ccce10acbfe68761364f57
Kevin Rhie <Linea89kr@gmail.com>
2023-07-27 08:58:38 +0900

    경로 수정 테스트1
    경로 수정 테스트1

---
+++ main/src/main/java/com/example/P1B/service/MypageService.java
@@ -0,0 +1,9 @@
+package com.example.P1B.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MypageService {
+}
