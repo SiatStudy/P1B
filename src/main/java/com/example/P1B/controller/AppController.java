72a42b6943d6cdba30b8c53f3f87e70b8f87f6cf
Linea89kr <Linea89kr@gmail.com>
2023-08-16 23:27:18 +0900

    [황수] Etag를 이용한 변경 감지 및 변경사항이 없을 경우를 대비한 리다이렉트 처리

--- src/main/java/com/example/P1B/controller/AppController.java
+++ src/main/java/com/example/P1B/controller/AppController.java
@@ -1,17 +1,38 @@
 package com.example.P1B.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.CacheControl;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class AppController {
-    @GetMapping("/loginpage")
-    public String loginpage(){
-        return "loginpage/index";
+    @RequestMapping(value = "/loginpage", produces = "text/html")
+    public String loginpage(HttpServletRequest request, HttpServletResponse response) {
+        String etag = "1234"; // 임의의 Etag 값
+        response.addHeader("ETag", etag); // ETag 헤더 설정
+        String ifNoneMatch = request.getHeader("If-None-Match"); // If-None-Match 헤더 값 가져오기
+        if (etag.equals(ifNoneMatch)) { // ETag 값이 일치하면
+            return "redirect:/loginpage"; // 302 리다이렉트 보내기
+        }else{
+            return "loginpage/index";
+        }
     }
 
-    @GetMapping("/mainpage")
-    public String mainpage(){
+
+    @RequestMapping(value = "/mainpage", produces = "text/html")
+    public String mainpage(HttpServletRequest request, HttpServletResponse response) {
+        String etag = "1234"; // 임의의 Etag 값
+        response.addHeader("ETag", etag); // ETag 헤더 설정
+        String ifNoneMatch = request.getHeader("If-None-Match"); // If-None-Match 헤더 값 가져오기
+        if (etag.equals(ifNoneMatch)) { // ETag 값이 일치하면
+            return "redirect:/mainpage"; // 302 리다이렉트 보내기
+        }
         return "mainpage/index";
     }
 }
